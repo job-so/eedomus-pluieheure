4f97425f559588a9a1de86494514b0a304177832
Olivier GROSJEANNE <developer@grosjeanne.name>
2020-08-13 10:51:23 +0200

    v2.0

--- pluiedanslheure.php
+++ pluiedanslheure.php
@@ -2,48 +2,71 @@
 /*************************************************************************************/
 /*         			### Script MAJ information pluie dans l'heure    ###             */
 /*                                                                                   */
-/*                     Développement par eedomusbox@gmail.com                        */
-/*                            Version 1.0                                            */
+/*                     Version 1.0 : eedomusbox@gmail.com                            */
+/*                     Version 2.0 : https://github.com/job-so/eedomus-pluieheure/   */
 /*************************************************************************************/
 
-// Définition de variable
+// DÃ©finition des variables
+$host="https://webservice.meteofrance.com/";
+$access_token = "__Wj7dVSTjV9YGu1guveLyDq0g7S7TfTjaHBTPTpO0kj8__";
+$localisation = '';
+$updated_on = '';
 $text = '';  
- 
-// Répération des paramètres 
-$ville =  getArg('ville');
 
-if ($ville == ''){$ville = '692720';} //Lyon
+//
+function sdk_getGPScoordinates($host, $access_token, $ville)
+{
+    $query=$host."places?token=".$access_token."&q=".$ville;
+    $jsonResponse = httpQuery($query);
+    $json = sdk_json_decode($jsonResponse, true);
+    return array (utf8_encode($json['0']['name']), $json['0']['lat'], $json['0']['lon']);
+}
 
-$query = "http://www.meteofrance.com/mf3-rpc-portlet/rest/pluie/".$ville;
-
-// Récupération des données
-
-$jsonResponse = httpQuery($query);
-
-//$response = file_get_contents($query);
-$json     = sdk_json_decode($jsonResponse, true);
-
-// Recherche des valeurs
-$Max = 0;  // Pas de connection
-foreach ($json['dataCadran'] as $dataCadran)
-	{ if ( $Max <= $dataCadran['niveauPluie']) 
-		{$Max = $dataCadran['niveauPluie'];}		
-	}
-	
-switch ($Max)
-		{ case '0': $text = 'Donnees indisponibles';break;
-		  case '1': $text = 'Pas de pluie'; break;
-		  case '2': $text = 'Pluie faible'; break; 
-		  case '3': $text = 'Pluie'; break; 
-		  case '4': $text = 'Pluie forte'; break; //on s'arrête complètement
-		  default : 
+//
+function sdk_getRainForecast($host, $access_token, $lat, $lon)
+{
+    $query=$host."rain?token=".$access_token."&lat=".$lat."&lon=".$lon;
+    $jsonResponse = httpQuery($query);
+	$json = sdk_json_decode($jsonResponse, true);
+	// Recherche des valeurs
+	$Max = 0;  // Pas de connection
+	foreach ($json['forecast'] as $dataCadran)
+		{ if ( $Max <= $dataCadran['rain']) 
+			{$Max = $dataCadran['rain'];}		
 		}
+		
+	switch ($Max)
+			{ case '0': $text = 'Donnees indisponibles';break;
+			case '1': $text = 'Pas de pluie'; break;
+			case '2': $text = 'Pluie faible'; break; 
+			case '3': $text = 'Pluie'; break; 
+			case '4': $text = 'Pluie forte'; break;
+			default : $text = 'Erreur';
+			}
+    return array ($json['updated_on'], $text);
+}
 
-// Génération du XML
+/*************************************************************************************/
+
+// RÃ©pÃ©ration des paramÃ¨tres 
+$ville =  getArg('ville');
+if ($ville == ''){$ville = '69000';} //Lyon
+
+// RÃ©cupÃ©ration des coordonnÃ©es GPS
+list ($localisation, $lat, $lon) = sdk_getGPScoordinates($host, $access_token,$ville);
+if ($localisation == '') {
+    $text = utf8_encode("ERREUR : ".$ville." non trouvÃ©(e)");
+} else {
+	// RÃ©cupÃ©ration du forecast de pluie
+	list($updated_on, $text) = sdk_getRainForecast($host, $access_token, $lat, $lon);
+}
+
+// GÃ©nÃ©ration du XML
 sdk_header('text/xml');
 $xml = '<?xml version="1.0" encoding="UTF-8"?>';
 $xml .= '<pluiedanslheure>';
-$xml .= '<heureMAJ>'.$json[lastUpdate].'</heureMAJ>';
+$xml .= '<localisation>'.$localisation.'</localisation>';
+$xml .= '<heureMAJ>'.$updated_on.'</heureMAJ>';
 $xml .= '<resultat>'.$text.'</resultat>';
 $xml .= '</pluiedanslheure>';
 echo $xml;
